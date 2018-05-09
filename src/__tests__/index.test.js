786c6325ae502d985ce0e20dc114854aa8539242
Julien TASSIN <julie@tassin.work>
2018-05-09 23:00:35 +0200

    first commit :tada:

--- src/__tests__/index.test.js
+++ src/__tests__/index.test.js
@@ -3,6 +3,17 @@ const http = require('http');
 const puppeteer = require('puppeteer');
 const port = 8907;
 const xhrBackendPort = 8908;
+const util = require('util');
+// const sleep = util.promisify(setTimeout);
+
+function timeout(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+async function sleep(delay) {
+  await timeout(delay);
+  return;
+}
+
 
 const OK_NO_XHR = `
 <html>
@@ -10,22 +21,75 @@ OK_NO_XHR
 </html>
 `;
 
+const OK_WITH_1_FAIL_XHR = `
+<html>
+OK_WITH_1_SLOW_XHR
+<script>
+  var xmlhttp = new XMLHttpRequest();
+  var url = 'http://localhost:${xhrBackendPort}/fail';
+  
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send(); 
+  
+</script>
+</html>
+`;
+
+const OK_WITH_1_XHR = `
+<html>
+OK_WITH_1_SLOW_XHR
+<script>
+  var xmlhttp = new XMLHttpRequest();
+  var url = 'http://localhost:${xhrBackendPort}';
+  
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send(); 
+  
+</script>
+</html>
+`;
+
 const OK_WITH_1_SLOW_XHR = `
 <html>
 OK_WITH_1_SLOW_XHR
+<script>
+setTimeout(() => {
+  var xmlhttp = new XMLHttpRequest();
+  var url = 'http://localhost:${xhrBackendPort}/infinite';
+  
+  xmlhttp.open("GET", url, true);
+  xmlhttp.send(); 
+}, 10);
+  
+</script>
 </html>
 `;
 
 const requestHandler = (request, response) => {
-  console.log(request);
-  response.statusCode = 200;
-  response.end(OK_NO_XHR);
+  if (request.url === '/with_infinite_xhr') {
+    response.statusCode = 200;
+    response.end(OK_WITH_1_SLOW_XHR);
+  } else if (request.url === '/with_xhr') {
+    response.statusCode = 200;
+    response.end(OK_WITH_1_XHR);
+  } else if (request.url === '/with_xhr_failing') {
+    response.statusCode = 200;
+    response.end(OK_WITH_1_XHR);
+  } else {
+    response.statusCode = 200;
+    response.end(OK_NO_XHR);
+  }
 };
 
 const backendRequestHandler = (request, response) => {
-  console.log(request);
-  response.statusCode = 200;
-  response.end({ status: 'OK' });
+  if (request.url === '/infinite') {
+    // This xhr will never end
+  } else if (request.url === '/fail') {
+    response.statusCode = 500;
+    response.end('boom');
+  } else {
+    response.end();
+  }
 }
 
 let server;
@@ -39,7 +103,11 @@ beforeEach(async () => {
   backendServer = http.createServer(backendRequestHandler);
   await backendServer.listen(xhrBackendPort);
 
-  browser = await puppeteer.launch({ headless: true });
+  const args = [];
+  browser = await puppeteer.launch({
+    headless: true,
+    args
+  });
 });
 
 afterEach(async () => {
@@ -51,25 +119,45 @@ describe('PendingXHR', () => {
 
   describe('pendingXhrCount', () => {
     it('returns 0 if no xhr pending count', async () => {
-      const page = await browser.newPage(`http://localhost:${port}/no_xhr`);
+      const page = await browser.newPage();
       const pendingXHR = new PendingXHR(page);
+      await page.goto(`http://localhost:${port}/no_xhr`);
       expect(pendingXHR.pendingXhrCount()).toEqual(0);
     });
 
     it('returns the xhr pending count', async () => {
-      const page = await browser.newPage(`http://localhost:${port}/with_xhr`);
+      const page = await browser.newPage();
       const pendingXHR = new PendingXHR(page);
+      await page.goto(`http://localhost:${port}/with_infinite_xhr`);
+      await sleep(20);
       expect(pendingXHR.pendingXhrCount()).toEqual(1);
     });
   });
 
   describe('waitForAllXhrFinished', async () => {
     it('returns immediatly if no xhr pending count', async () => {
-      const page = await browser.newPage(`http://localhost:${port}`);
+      const page = await browser.newPage();
       const pendingXHR = new PendingXHR(page);
+      await page.goto(`http://localhost:${port}/no_xhr`);
       await pendingXHR.waitForAllXhrFinished();
     });
-    
-  });
 
+    it('waits for all xhr to end', async () => {
+      const page = await browser.newPage();
+      const pendingXHR = new PendingXHR(page);
+      await page.goto(`http://localhost:${port}/with_xhr`);
+      expect(pendingXHR.pendingXhrCount()).toEqual(1);
+      await pendingXHR.waitForAllXhrFinished();
+      expect(pendingXHR.pendingXhrCount()).toEqual(0);
+    });
+
+    it('handle correctly failed xhr', async () => {
+      const page = await browser.newPage();
+      const pendingXHR = new PendingXHR(page);
+      await page.goto(`http://localhost:${port}/with_xhr_failing`);
+      expect(pendingXHR.pendingXhrCount()).toEqual(1);
+      await pendingXHR.waitForAllXhrFinished();
+      expect(pendingXHR.pendingXhrCount()).toEqual(0);
+    });
+  });
 });
--- src/index.js
+++ src/index.js
@@ -2,24 +2,47 @@ class PendingXHR {
 
   constructor(page) {
     this.page = page;
+    this.resourceType = 'xhr';
+    // page.setRequestInterception(true);
     this.pendingXhrs = new Set();
     this.finishedWithSuccessXhrs = new Set();
     this.finishedWithErrorsXhrs = new Set();
     page.on('request', request => {
-      if (request.resourceType() === 'xhr') {
-        this.pendingXhrs.push(request);
+      console.log(Date.now(), 'new request', request.resourceType(), ' to ', request.url());
+      if (request.resourceType() === this.resourceType) {
+        this.pendingXhrs.add(request);
+        if(!this.promise) {
+          this.promise = new Promise((resolve, reject) => {
+            this.resolver = resolve;
+          });
+          this.promise.then(() => {
+            console.log('All xhr are ended');
+          });
+        }       
       }
     });
     page.on('requestfailed', request => {
-      if (request.resourceType() === 'xhr') {
+      if (request.resourceType() === this.resourceType) {
+        console.log(this.pendingXhrs);
         this.pendingXhrs.delete(request);
-        this.finishedWithErrorsXhrs.push(request);
+        this.finishedWithErrorsXhrs.add(request);
+        if(this.resolver) {
+          this.resolver();
+          delete this.promise;
+          delete this.resolver;
+        }
       }
     });
     page.on('requestfinished', request => {
-      if (request.resourceType() === 'xhr') {
+      console.log(Date.now(), 'request finished', request.resourceType(), ' to ', request.url());
+      if (request.resourceType() === this.resourceType) {
         this.pendingXhrs.delete(request);
-        this.finishedWithSuccessXhrs.push(request);
+        this.finishedWithSuccessXhrs.add(request);
+        if(this.resolver) {
+          this.resolver();
+          delete this.promise;
+          delete this.resolver;
+        }
       }
     });
   }
@@ -27,6 +50,10 @@ class PendingXHR {
   async waitForAllXhrFinished() {
     if (this.pendingXhrCount() === 0) {
       return true;
+    }
+    if(this.promise) {
+      console.log('promise exists', this.promise);
+      await this.promise;
     }
   }
 
