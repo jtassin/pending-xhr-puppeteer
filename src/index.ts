895ca96396f8822105563e6cf39513c7112420c4
PsAiL <allenps1991@hotmail.com>
2020-06-16 03:02:02 +0000

    feat: refactor some logic in src/index.ts. Mainly fix waitOnceForAllXhrFinished may throw error which casuses memory leak. rewrite listeners as arrow method. Expose removeEventListeners for developer to control it

--- src/index.ts
+++ src/index.ts
@@ -4,24 +4,17 @@ interface ResolvableRequest extends Request {
   resolver: () => void;
 }
 
-export class PendingXHR {
+/**
+ * 魔改来源: https://github.com/jtassin/pending-xhr-puppeteer
+ */
+// noinspection SpellCheckingInspection
+export default class PendingXHR {
   page: Page;
-
   resourceType: string;
-
   pendingXhrs: Set<Request>;
-
   finishedWithSuccessXhrs: Set<Request>;
-
   finishedWithErrorsXhrs: Set<Request>;
-
-  promisees: Promise<void>[];
-
-  requestListener: (request: ResolvableRequest) => void;
-
-  requestFailedListener: (request: ResolvableRequest) => void;
-
-  requestFinishedListener: (request: ResolvableRequest) => void;
+  promisees: Array<Promise<void>>
 
   constructor(page: Page) {
     this.promisees = [];
@@ -31,56 +24,76 @@ export class PendingXHR {
     this.finishedWithSuccessXhrs = new Set();
     this.finishedWithErrorsXhrs = new Set();
 
-    this.requestListener = (request: ResolvableRequest) => {
-      if (request.resourceType() === this.resourceType) {
-        this.pendingXhrs.add(request);
-        this.promisees.push(
-          new Promise(resolve => {
-            request.resolver = resolve;
-          }),
-        );
-      }
-    };
-
-    this.requestFailedListener = (request: ResolvableRequest) => {
-      if (request.resourceType() === this.resourceType) {
-        this.pendingXhrs.delete(request);
-        this.finishedWithErrorsXhrs.add(request);
-        request.resolver();
-        delete request.resolver;
-      }
-    };
-
-    this.requestFinishedListener = (request: ResolvableRequest) => {
-      if (request.resourceType() === this.resourceType) {
-        this.pendingXhrs.delete(request);
-        this.finishedWithSuccessXhrs.add(request);
-        request.resolver();
-        delete request.resolver;
-      }
-    };
-
-    page.on('request', this.requestListener);
-    page.on('requestfailed', this.requestFailedListener);
-    page.on('requestfinished', this.requestFinishedListener);
+    this.addEventListeners();
   }
 
-  async waitForAllXhrFinished() {
+  private addEventListeners() {
+    this.page.on('request', this.requestListener);
+    this.page.on('requestfailed', this.requestFailedListener);
+    this.page.on('requestfinished', this.requestFinishedListener);
+  }
+
+  /**
+   * expose this allow developer manually control it
+   */
+  public removeEventListeners() {
+    this.page.removeListener('request', this.requestListener);
+    this.page.removeListener('requestfailed', this.requestFailedListener);
+    this.page.removeListener('requestfinished', this.requestFinishedListener);
+  }
+
+  private requestListener = (request: ResolvableRequest) => {
+    if (request.resourceType() === this.resourceType) {
+      this.pendingXhrs.add(request);
+      this.promisees.push(
+        new Promise(resolve => {
+          request.resolver = resolve;
+        }),
+      );
+    }
+  }
+
+  private requestFailedListener = (request: ResolvableRequest) => {
+    if (request.resourceType() === this.resourceType) {
+      this.pendingXhrs.delete(request);
+      this.finishedWithErrorsXhrs.add(request);
+      if (request.resolver) {
+        request.resolver();
+        delete request.resolver;
+      }
+    }
+  };
+
+  private requestFinishedListener = (request: ResolvableRequest) => {
+    if (request.resourceType() === this.resourceType) {
+      this.pendingXhrs.delete(request);
+      this.finishedWithSuccessXhrs.add(request);
+      if (request.resolver) {
+        request.resolver();
+        delete request.resolver;
+      }
+    }
+  };
+
+  public async waitForAllXhrFinished() {
     if (this.pendingXhrCount() === 0) {
       return;
     }
     await Promise.all(this.promisees);
   }
 
-  async waitOnceForAllXhrFinished() {
-    await this.waitForAllXhrFinished();
-
-    this.page.removeListener('request', this.requestListener);
-    this.page.removeListener('requestfailed', this.requestFailedListener);
-    this.page.removeListener('requestfinished', this.requestFinishedListener);
+  public async waitOnceForAllXhrFinished() {
+    /**
+     * try catch ensure anyhow remove event listeners
+     */
+    try {
+      await this.waitForAllXhrFinished();
+    } finally {
+      this.removeEventListeners();
+    }
   }
 
-  pendingXhrCount() {
+  public pendingXhrCount() {
     return this.pendingXhrs.size;
   }
 }
